ecda4126f400d5d7645b93962f57d84309f447c3
Ryan Edmondson <ryan@saladcreative.com>
2019-10-29 16:49:06 +0000

    Disable block editor on posts

--- functions/utilities.php
+++ functions/utilities.php
@@ -19,3 +19,7 @@ if (!current_user_can('manage_options')) {
 if (!current_user_can('edit_posts')) {
     add_filter('show_admin_bar', '__return_false');
 }
+
+// disable block editor on posts and custom post types
+add_filter('use_block_editor_for_post', '__return_false', 10);
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
